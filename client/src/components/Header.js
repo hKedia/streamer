558aca17ea9c91058c345677aace9c7b5c2d7dd2
Harsh Kedia <harshkedia@outlook.in>
2020-03-04 21:47:38 +0100

    OAuth with React

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
 
 const Header = () => {
 	return (
 		<div className="ui secondary pointing menu">
 			<Link to="/" className="item">
-				Streamer
-      </Link>
+				<span>Streamer</span>
+			</Link>
 			<div className="right menu">
 				<Link to="/" className="item">
-					All Streams
-        </Link>
+					<span>All Streams</span>
+				</Link>
+				<GoogleAuth />
 			</div>
 		</div>
 	);
